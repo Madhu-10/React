add955a6331edcef29e529dddd44283e35ab380f
Madhu-10 <madhukavi10052003@gmail.com>
2024-05-13 16:51:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,63 +1,70 @@
-import { useState } from 'react';
-// import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
 
-function App(){
-    
-    const formSubmitted=(event)=>{
-        alert("Form submitted")
-        event.preventDefault()
-    }
-    const [fruit, setFruit]=useState("mango")
-    const [gender ,setGender]=useState(null)
-    let hobbies=[]
-    const setHobby = (event)=>{
-        if(event.target.checked){
-            hobbies.push(event.target.value);
+function Comments(){
+    const [comments, SetComments] = useState(null)
+    useEffect(
+        ()=>{
+           fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
+                .then(response => response.json())
+                .then((json) => {
+                    SetComments(json)
+                    console.log(json)
+                })  
         }
-        else{
-            hobbies = hobbies.filter((item)=>{
-                if(item!==event.target.value){
-                    return item
-                }
-            })
-        }
-        console.log(hobbies);
-    }
-
+    )
     return(
         <>
-        <h1>Sign up form</h1>
-        <form method='GET' onSubmit={formSubmitted}>
-            <input type='text' name='username' placeholder='Username' /><br />
-            <input type='email' name='email' placeholder='email@example.com' /><br />
-            <input type='contact' name='contact' placeholder='9566444576' /><br />
-            <input type='password' name='password' placeholder='Password' /><br />
-            <input type='submit' value="Submit" />
-        </form>
-
-        <select value={fruit} onChange={(e)=>{setFruit(e.target.value)}}>
-            <option value="apple">Apple</option>
-            <option value="orange">Orange</option>
-            <option value="mango">Mango</option>
-        </select>
-        {
-            <p>The Selected Fruit is {fruit}</p>
-        }
-
-        <h3>Select Gender</h3>
-        <input type="radio" name="gender" value="Male" onClick={(e)=>{setGender(e.target.value)}} /> Male<br />
-        <input type="radio" name="gender" value="Female" onClick={(e)=>{setGender(e.target.value)}} /> Female<br />
-        <input type="radio" name="gender" value="Other" onClick={(e)=>{setGender(e.target.value)}} /> Other<br />
-        {
-            <p>Selected Gender is {gender}</p>
-        }
-        <h3>Select Hobbies</h3>
-        <input type="checkbox" value="cricket" onChange={(e)=>{setHobby(e)}} />Cricket<br />
-        <input type="checkbox" value="football" onChange={(e)=>{setHobby(e)}} />Football<br />
-        <input type="checkbox" value="volleyball" onChange={(e)=>{setHobby(e)}} />Volleyball<br />
-        
-
+            <h1>These are Comments</h1>
+            {
+                comments &&
+                comments.map((e)=>{
+                    return <>
+                        <p style ={{color:"red"}}>{e.postId} . {e.name}</p><br />
+                        {e.body} <br />
+                    </>
+                })
+            }
         </>
     )
 }
-export default App
+
+function App(){
+    const [showComments, setShowComments] = useState(false)
+    return(
+        <>
+            <input type="button" value="Show Comments" onClick={ (e) => {setShowComments(!showComments)}} />
+            {
+                showComments &&
+                <Comments />
+            }
+        </>
+    )
+}
+export default App;
+
+// import { useEffect } from "react";
+// import { useState } from "react";
+
+// function Counter({count}){
+//     useEffect(
+//         ()=>{
+//             console.log("Component Rendering...");
+//         }
+//     ,[count])//props and state variable can be watched //count ah matu del panita ovoru increment kum component rendering varadhu
+//     return(
+//         <>
+//             The counter value is {count}
+//         </>
+//     )
+// }
+// function App(){
+//     const [count, setCount] = useState(0)
+//     return(
+//         <>
+//             <input type="button" value="Increment" onClick={ () => {setCount(count+1)}} /><br />
+//             <Counter count={count} />
+//         </>
+//     )
+// }
+// export default App
